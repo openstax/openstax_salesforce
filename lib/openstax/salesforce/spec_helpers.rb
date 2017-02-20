f35eb0ab0a45cc912a89cdbf77571f76bf0801fe
JP Slavinsky <jpslav@gmail.com>
2017-02-20 13:17:41 -0800

    0.10.0

--- lib/openstax/salesforce/spec_helpers.rb
+++ lib/openstax/salesforce/spec_helpers.rb
@@ -25,6 +25,32 @@ module OpenStax::Salesforce::SpecHelpers
     OpenStax::Salesforce::User.destroy_all
   end
 
+  # Uses knowledge of how `SalesforceProxy` methods create new SF records using its
+  # `unique_token` to create the conditions you'd otherwise have to manually set
+  # when calling `limit_salesforce_queries`.
+  #
+  def limit_salesforce_queries_by_token(remote_class, token)
+    case remote_class.new
+    when OpenStax::Salesforce::Remote::Contact, OpenStax::Salesforce::Remote::Lead
+      limit_salesforce_queries(remote_class, last_name: "%#{token}")
+    else
+      raise "Don't know how to apply to #{remote_class}"
+    end
+  end
+
+  # Often, when dealing with Salesforce sandboxes, there are a bunch of other records
+  # in the sandbox that you'd rather your specs ignore.  This method makes SF queries
+  # in your specs have additional `where` clauses to limit what is returned.  If you
+  # are careful how you create the records you are testing against, you can use this
+  # method to only consider those records.  E.g. when creating a contact, if you make
+  # each of the last names end with "_foobar", then by saying:
+  #
+  #   limit_salesforce_queries(OpenStax::Salesforce::Remote::Contact, last_name: "%_foobar")
+  #
+  # your Contact queries will only return the one you made with the last name ending
+  # in "_foobar".  The methods in SalesforceProxy that help you make SF records
+  # use the `unique_token` in this way (set via `reset_unique_token`).
+  #
   def limit_salesforce_queries(remote_class, **conditions)
     allow(remote_class).to receive(:query) do
       like_conditions = {}
