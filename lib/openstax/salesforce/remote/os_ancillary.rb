dd182436fe1bb22d6093bfbf40e979d87f6ccfde
JP Slavinsky <jpslav@gmail.com>
2017-06-10 14:59:10 -0700

    New IA and OSA fields

--- lib/openstax/salesforce/remote/os_ancillary.rb
+++ lib/openstax/salesforce/remote/os_ancillary.rb
@@ -5,10 +5,11 @@ module OpenStax::Salesforce::Remote
     field :product,                   from: "Product__c", as: :picklist
     field :account_type,              from: "Account_Type__c"
     field :book_name,                 from: "Book_Name__c"
-    field :course_start_date,         from: "Class_Start_Date__c", as: :date
+    field :course_start_date,         from: "Course_Start_Date__c", as: :date
     field :course_name,               from: "Course_Name__c"
     field :course_code,               from: "Course_Code__c"
     field :course_id,                 from: "External_ID__c"
+    field :course_uuid,               from: "External_UUID__c"
     field :created_at,                from: "E_Created_Date__c", as: :datetime
     field :error,                     from: "Error__c"
     field :general_access_url,        from: "General_Access_URL__c"
@@ -17,8 +18,12 @@ module OpenStax::Salesforce::Remote
     field :num_teachers,              from: "Active_Teachers__c", as: :int
     field :num_sections,              from: "Sections__c", as: :int
     field :num_students,              from: "Students_Using__c", as: :int
+    field :num_students_paid,         from: "Students_Paid__c", as: :int
+    field :num_students_comped,       from: "Students_Comped__c", as: :int
+    field :num_students_refunded,     from: "Students_Refunded__c", as: :int
     field :term_year,                 from: "TermYear__c"
     field :term,                      from: "Term__c"
+    field :estimated_enrollment,      from: "Estimated_Enrollment__c", as: :int
     field :opportunity_id,            from: 'Opportunity__c'
     field :individual_adoption_id,    from: "Individual_Adoption__c"
     field :contact_id,                from: "Contact__c"
@@ -29,6 +34,9 @@ module OpenStax::Salesforce::Remote
 
     def reset_stats
       self.num_students = 0
+      self.num_students_paid = 0
+      self.num_students_comped = 0
+      self.num_students_refunded = 0
       self.num_teachers = 0
       self.num_sections = 0
     end
