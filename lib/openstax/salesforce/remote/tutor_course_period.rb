eda7978c4367cdf2516b14643bf7e9475394b0c6
Dante Soares <dante.m.soares@rice.edu>
2018-09-13 21:07:21 -0500

    Renamed TutorCourse to TutorCoursePeriod and tweaked some fields

--- lib/openstax/salesforce/remote/tutor_course_period.rb
+++ lib/openstax/salesforce/remote/tutor_course_period.rb
@@ -1,7 +1,8 @@
 module OpenStax::Salesforce::Remote
-  class TutorCourse < ActiveForce::SObject
+  class TutorCoursePeriod < ActiveForce::SObject
 
     field :num_teachers,              from: "Active_Teachers__c", as: :int
+    field :base_year,                 from: "Base_Year__c", as: :int
     field :book_name,                 from: "Book_Name__c"
     field :contact_id,                from: "Contact__c"
     field :course_code,               from: "Course_Code__c"
@@ -13,11 +14,10 @@ module OpenStax::Salesforce::Remote
     field :estimated_enrollment,      from: "Estimated_Enrollment__c", as: :int
     field :course_id,                 from: "External_ID__c"
     field :course_uuid,               from: "External_UUID__c"
-    field :latest_adoption_decision,  from: "latest_adoption_decision__c"
+    field :latest_adoption_decision,  from: "Latest_Adoption_Decision__c"
     field :does_cost,                 from: "Paid_Course__c", as: :boolean
     field :is_research,               from: "Research_Project__c", as: :boolean
-    field :school_year,               from: "School_Year__c"
-    field :num_sections,              from: "Sections__c", as: :int
+    field :num_periods,               from: "Sections__c", as: :int
     field :status,                    from: "Status__c", as: :picklist
     field :num_students_comped,       from: "Students_Comped__c", as: :int
     field :num_students_dropped,      from: "Students_Dropped__c", as: :int
@@ -25,14 +25,15 @@ module OpenStax::Salesforce::Remote
     field :num_students_refunded,     from: "Students_Refunded__c", as: :int
     field :num_students,              from: "Students_Using__c", as: :int
     field :num_students_with_work,    from: "Students_With_Work__c", as: :int
+    field :period_uuid,               from: "Period_UUID__c"
     field :term,                      from: "Term__c"
     field :is_pilot,                  from: "Tutor_Pilot__c", as: :boolean
 
     self.table_name = 'Tutor_Course__c'
 
     def reset_stats
+      self.num_periods = 0
       self.num_teachers = 0
-      self.num_sections = 0
       self.num_students_comped = 0
       self.num_students_dropped = 0
       self.num_students_paid = 0
@@ -42,6 +43,7 @@ module OpenStax::Salesforce::Remote
     end
 
     STATUS_APPROVED = "Approved"
+    STATUS_ARCHIVED = "ArchivedPeriod"
 
   end
 end
