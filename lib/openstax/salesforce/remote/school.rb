61d82df4c2b95ae9164872132d1241abf9e7988f
Dante Soares <dante.m.soares@rice.edu>
2020-04-22 10:24:20 -0500

    Added is_kip field to schools

--- lib/openstax/salesforce/remote/school.rb
+++ lib/openstax/salesforce/remote/school.rb
@@ -1,6 +1,7 @@
 module OpenStax::Salesforce::Remote
   class School < ActiveForce::SObject
-    field :name,                      from: "Name"
+    field :name,   from: 'Name'
+    field :is_kip, from: 'K_I_P__c', as: :boolean
 
     self.table_name = 'Account'
   end
