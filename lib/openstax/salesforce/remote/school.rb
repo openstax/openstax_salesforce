0ee73a2c5476a62e7a0b08dee0a3688f9e388db5
Dante Soares <dante.m.soares@rice.edu>
2020-05-28 14:45:08 -0500

    Added type, school_location, is_child_of_kip to OpenStax::Salesforce::Remote::School

--- lib/openstax/salesforce/remote/school.rb
+++ lib/openstax/salesforce/remote/school.rb
@@ -1,7 +1,10 @@
 module OpenStax::Salesforce::Remote
   class School < ActiveForce::SObject
-    field :name,   from: 'Name'
-    field :is_kip, from: 'K_I_P__c', as: :boolean
+    field :name,            from: 'Name'
+    field :type,            from: 'Type'
+    field :school_location, from: 'School_Location__c'
+    field :is_kip,          from: 'K_I_P__c',        as: :boolean
+    field :is_child_of_kip, from: 'child_of_kip__c', as: :boolean
 
     self.table_name = 'Account'
   end
