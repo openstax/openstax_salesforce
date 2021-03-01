1695a0dceef0969e3400e633743a5ab5f3b6b9ef
Dante Soares <dante.m.soares@rice.edu>
2021-03-01 16:20:48 -0600

    Added sheerid_school_name to Schools

--- lib/openstax/salesforce/remote/school.rb
+++ lib/openstax/salesforce/remote/school.rb
@@ -1,10 +1,11 @@
 module OpenStax::Salesforce::Remote
   class School < ActiveForce::SObject
-    field :name,            from: 'Name'
-    field :type,            from: 'Type'
-    field :school_location, from: 'School_Location__c'
-    field :is_kip,          from: 'K_I_P__c',        as: :boolean
-    field :is_child_of_kip, from: 'child_of_kip__c', as: :boolean
+    field :name,                from: 'Name'
+    field :type,                from: 'Type'
+    field :school_location,     from: 'School_Location__c'
+    field :sheerid_school_name, from: 'SheedID_School_Name__c'
+    field :is_kip,              from: 'K_I_P__c',        as: :boolean
+    field :is_child_of_kip,     from: 'child_of_kip__c', as: :boolean
 
     self.table_name = 'Account'
   end
