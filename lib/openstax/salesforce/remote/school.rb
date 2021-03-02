faccdd8c89189b50c9750c0a316c8ed0ae5ad35b
Dante Soares <dante.m.soares@rice.edu>
2021-03-02 16:26:20 -0600

    Updated mispelled SF field with fixed name

--- lib/openstax/salesforce/remote/school.rb
+++ lib/openstax/salesforce/remote/school.rb
@@ -5,7 +5,7 @@ module OpenStax::Salesforce::Remote
     field :state,               from: 'BillingState'
     field :type,                from: 'Type'
     field :school_location,     from: 'School_Location__c'
-    field :sheerid_school_name, from: 'SheedID_School_Name__c'
+    field :sheerid_school_name, from: 'SheerID_School_Name__c'
     field :is_kip,              from: 'K_I_P__c',        as: :boolean
     field :is_child_of_kip,     from: 'child_of_kip__c', as: :boolean
 
