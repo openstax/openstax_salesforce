53c595bd026a090fb5b41213b329695f6efc9cab
Dante Soares <dante.m.soares@rice.edu>
2021-03-16 16:25:01 -0500

    Added annualization_number to books

--- lib/openstax/salesforce/remote/book.rb
+++ lib/openstax/salesforce/remote/book.rb
@@ -1,6 +1,7 @@
 module OpenStax::Salesforce::Remote
   class Book < ActiveForce::SObject
-    field :name,                      from: "Name"
+    field :name,                 from: "Name"
+    field :annualization_number, from: "Annualization_number__c"
 
     self.table_name = 'Book__c'
   end
--- lib/openstax/salesforce/version.rb
+++ lib/openstax/salesforce/version.rb
@@ -1,5 +1,5 @@
 module OpenStax
   module Salesforce
-    VERSION = '5.1.0'
+    VERSION = '5.1.1'
   end
 end
