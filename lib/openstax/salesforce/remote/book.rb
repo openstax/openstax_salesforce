8547001ed96a08e8cce224358560d3619fd70a5d
Dante Soares <dante.m.soares@rice.edu>
2021-03-16 16:52:16 -0500

    Made the annualization_number field a decimal and added it to the spec_helper

--- lib/openstax/salesforce/remote/book.rb
+++ lib/openstax/salesforce/remote/book.rb
@@ -1,7 +1,7 @@
 module OpenStax::Salesforce::Remote
   class Book < ActiveForce::SObject
     field :name,                 from: "Name"
-    field :annualization_number, from: "Annualization_number__c"
+    field :annualization_number, from: "Annualization_number__c", as: :decimal
 
     self.table_name = 'Book__c'
   end
