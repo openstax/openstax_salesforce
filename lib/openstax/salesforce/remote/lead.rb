8b8281299153f38467f439f9f26ef5726f984b32
Dante Soares <dante.m.soares@rice.edu>
2021-04-07 11:58:07 -0500

    Added country fields to Lead and School

--- lib/openstax/salesforce/remote/lead.rb
+++ lib/openstax/salesforce/remote/lead.rb
@@ -19,6 +19,7 @@ module OpenStax::Salesforce::Remote
     field :city,                from: "City"
     field :state,               from: "State"
     field :state_code,          from: "StateCode"
+    field :country,             from: "Country"
     field :phone,               from: "Phone"
     field :website,             from: "Website"
     field :status,              from: "Status"
