16f3e31d13cc8b958f83338e44aa680745977714
Michael Harrison <mwharrison@rice.edu>
2022-04-04 15:41:40 -0500

    add official name, id to book object from salesforce

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -4,6 +4,8 @@ FactoryBot.define do
 
     id { Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3) }
     name { Faker::Book.title }
+    official_name { Faker::Book.title }
+    annualization_number { Faker::Number.decimal(l_digits: 5, r_digits: 2) }
 
   end
 end
