5d149d74fbe2e93f4bd2e2bb3b8f1a7d419eafa5
Michael Harrison <volo@rice.edu>
2023-11-17 12:14:17 -0600

    let salesforce handle the validations

--- lib/openstax/salesforce/remote/lead.rb
+++ lib/openstax/salesforce/remote/lead.rb
@@ -44,10 +44,7 @@ module OpenStax
         # These 2 fields both hold the Account (School) ID, but have different data types and uses in SF
         field :account_id,          from: 'Account_ID__c'
         field :school_id,           from: 'School__c'
-
-        validates(:last_name, presence: true)
-        validates(:school, presence: true)
-
+        
         self.table_name = 'Lead'
 
       end
